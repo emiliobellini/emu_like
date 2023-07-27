bb0b15109aeeebb760641b264400b9ba71b712a7
Emilio Bellini <emilio.bellini@protonmail.com>
2023-07-27 16:13:20 +0100

    added decorators to custom loss functions

--- tools/loss_functions.py
+++ tools/loss_functions.py
@@ -4,23 +4,28 @@ Loss function should take as input y_true and y_predicted
 and a float as output
 """
 from tensorflow.python.keras import backend as keras_backend
+from tensorflow import keras
 
 
+@keras.saving.register_keras_serializable()
 def max_absolute_error(y_true, y_pred):
     diff = keras_backend.abs(y_true - y_pred)
     return keras_backend.max(diff, axis=0)
 
 
+@keras.saving.register_keras_serializable()
 def mean_absolute_error(y_true, y_pred):
     diff = keras_backend.abs(y_true - y_pred)
     return keras_backend.mean(diff, axis=0)
 
 
+@keras.saving.register_keras_serializable()
 def max_relative_error(y_true, y_pred):
     diff = keras_backend.abs(y_true/y_pred - 1.)
     return keras_backend.max(diff, axis=0)
 
 
+@keras.saving.register_keras_serializable()
 def mean_relative_error(y_true, y_pred):
-    diff = keras_backend.abs(y_true/y_pred - 1.)
+    diff = keras_backend.abs((y_true - y_pred)/y_true)
     return keras_backend.mean(diff, axis=0)
