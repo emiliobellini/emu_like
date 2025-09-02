cbd7d1845e3dfce98acfbb9d4836f255e1d48dd4
Emilio Bellini <emilio.bellini@protonmail.com>
2025-09-02 14:43:56 +0200

    check_training_status

---
+++ scripts/check_training_status.py
@@ -0,0 +1,86 @@
+import argparse
+import os
+import re
+import emu_like.io as io
+
+def get_head(fname, lines=1):
+    """
+    Imitates the bash head command
+    """
+    with open(fname, 'r') as f:
+        f.seek(0, 2)     # go to end of file
+        total_bytes = f.tell()
+        lines_found, total_bytes_scanned = 0, 0
+        while (lines+1 > lines_found and
+                total_bytes > total_bytes_scanned):
+            byte_block = min(1024, total_bytes-total_bytes_scanned)
+            f.seek(total_bytes_scanned, 0)
+            total_bytes_scanned += byte_block
+            lines_found += f.read(byte_block).count('\n')
+        f.seek(0, 0)
+        line_list = list(f.readlines(total_bytes_scanned))
+        line_list = [x.rstrip() for x in line_list[:lines]]
+    return line_list
+
+def get_tail(fname, lines=1):
+    """
+    Imitates the bash tail command
+    """
+    # Main body
+    with open(fname, 'r') as f:
+        f.seek(0, 2)     # go to end of file
+        total_bytes = f.tell()
+        lines_found, total_bytes_scanned = 0, 0
+        while (lines+1 > lines_found and
+                total_bytes > total_bytes_scanned):
+            byte_block = min(1024, total_bytes-total_bytes_scanned)
+            f.seek(total_bytes-total_bytes_scanned-byte_block, 0)
+            try:
+                lines_found += f.read(byte_block).count('\n')
+            except UnicodeDecodeError:
+                pass
+            total_bytes_scanned += byte_block
+        f.seek(total_bytes-total_bytes_scanned, 0)
+        line_list = list(f.readlines())
+        line_list = [x.rstrip() for x in line_list[-lines:]]
+    return line_list
+
+
+# -----------------MAIN-CALL-----------------------------------------
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('log_files', nargs='+')
+    args = parser.parse_args()
+
+    str_out = '\x1b[1;32m[info]\x1b[00m Writing output in '
+
+    for log_file in args.log_files:
+        # Locate output file
+        head = get_head(log_file, lines=40)
+        for line in head:
+            if line.startswith(str_out):
+                output_folder = line.replace(str_out, '')
+
+        # Get last epoch
+        tail = get_tail(log_file, lines=1000)
+        for line in tail:
+            if re.match('Epoch [0-9]+/[0-9]+', line):
+                epochs = re.sub('Epoch ', '', line)
+                last_epoch, tot_epochs = epochs.split('/')
+                last_epoch = int(last_epoch)
+                tot_epochs = int(tot_epochs)
+        
+        # Get last improvement epoch
+        tail = get_tail(os.path.join(output_folder, 'history_log.cvs'), lines=1)[0]
+        best_epoch, loss, val_loss = tail.split(',')
+        best_epoch = int(best_epoch)
+        loss = float(loss)
+        val_loss = float(val_loss)
+
+        # Print stuff
+        io.info('Folder {}'.format(output_folder))
+        io.print_level(1, 'Last epoch: {}/{}'.format(last_epoch, tot_epochs))
+        io.print_level(1, 'Best epoch: {}'.format(best_epoch))
+        io.print_level(1, 'Epochs without improvement: {}'.format(last_epoch-best_epoch))
+        io.print_level(1, 'Loss: {}'.format(loss))
+        io.print_level(1, 'Validation Loss: {}'.format(val_loss))
