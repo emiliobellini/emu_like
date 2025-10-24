7f814065dfe4a8586c7c7cad72fa9b73ae9ed0f9
Emilio Bellini <emilio.bellini@protonmail.com>
2025-10-24 10:33:13 +0200

    .

--- pipelines/train.py
+++ pipelines/train.py
@@ -50,16 +50,21 @@ def train_emu(args):
                 'precious data! If you want to resume a previous run use '
                 'the --resume (-r) option.')
 
+    # Local variables
+    pars_out = params['output']
+    pars_emu = params['emulator']
+    pars_dat = params['datasets']
+
     # Call the right emulator
     emu = Emulator.choose_one(
-        params['emulator']['name'],
+        pars_emu['name'],
         verbose=args.verbose)
 
     # Test datasets input paths
-    has_paths = params['datasets']['paths'] is not None
+    has_paths = pars_dat['paths'] is not None
     if has_paths:
         try:
-            all([io.FitsFile(p) for p in params['datasets']['paths']])
+            all([io.FitsFile(p) for p in pars_dat['paths']])
             paths_is_fits = True
         except ValueError:
             paths_is_fits = False
@@ -71,43 +76,43 @@ def train_emu(args):
     if has_paths and paths_is_fits:
         data = [Dataset().load(
             path=path,
-            name=params['datasets']['name'],
-            columns_x=params['datasets']['columns_x'],
-            columns_y=params['datasets']['columns_y'],
+            name=pars_dat['name'],
+            columns_x=pars_dat['columns_x'],
+            columns_y=pars_dat['columns_y'],
             verbose=False)
-            for path in params['datasets']['paths']]
+            for path in pars_dat['paths']]
     # 2) unique text files for x and y
     elif has_paths:
         data = [Dataset().load_external(
             path=path,
-            columns_x=params['datasets']['columns_x'],
-            columns_y=params['datasets']['columns_y'],
+            columns_x=pars_dat['columns_x'],
+            columns_y=pars_dat['columns_y'],
             verbose=False)
-            for path in params['datasets']['paths']]
+            for path in pars_dat['paths']]
     # 3) separate text files for x and y
     else:
         data = [Dataset().load_external(
             path=path_x,
             path_y=path_y,
-            columns_x=params['datasets']['columns_x'],
-            columns_y=params['datasets']['columns_y'],
+            columns_x=pars_dat['columns_x'],
+            columns_y=pars_dat['columns_y'],
             verbose=False)
             for path_x, path_y in zip(
-                params['datasets']['paths_x'], params['datasets']['paths_y'])]
+                pars_dat['paths_x'], pars_dat['paths_y'])]
 
     # Remove non finite "y"
-    if params['datasets']['remove_non_finite']:
+    if pars_dat['remove_non_finite']:
         data = [d.remove_non_finite(verbose=False) for d in data]
 
     # Print info
     if args.verbose:
         io.info('Datasets arguments')
-        io.print_level(1, 'Name: {}.'.format(params['datasets']['name']))
+        io.print_level(1, 'Name: {}.'.format(pars_dat['name']))
         io.print_level(1, 'Sliced x data with columns: {}.'.format(
-            params['datasets']['columns_x']))
+            pars_dat['columns_x']))
         io.print_level(1, 'Sliced y data with columns: {}.'.format(
-            params['datasets']['columns_y']))
-        if params['datasets']['remove_non_finite']:
+            pars_dat['columns_y']))
+        if pars_dat['remove_non_finite']:
             io.print_level(1, 'Removing non finite y from dataset.')
 
     # Join all datasets
@@ -115,41 +120,40 @@ def train_emu(args):
 
     # Split training and testing samples
     data.train_test_split(
-        params['datasets']['frac_train'],
-        params['datasets']['train_test_random_seed'],
+        pars_dat['frac_train'],
+        pars_dat['train_test_random_seed'],
         verbose=args.verbose)
 
     # If requested, rescale training and testing samples
     data.rescale(
-        params['datasets']['rescale_x'],
-        params['datasets']['rescale_y'],
+        pars_dat['rescale_x'],
+        pars_dat['rescale_y'],
         verbose=args.verbose)
 
     # If requested apply PCA on x and/or y
     data.apply_pca(
-        params['datasets']['num_x_pca'],
-        params['datasets']['num_y_pca'],
+        pars_dat['num_x_pca'],
+        pars_dat['num_y_pca'],
         verbose=args.verbose)
 
     # If resume
     if args.resume:
         # Load emulator
-        emu.load(params['output']['path'], model_to_load='best',
-                 verbose=args.verbose)
+        emu.load(pars_out['path'], model_to_load='best', verbose=args.verbose)
     # Otherwise
     else:
         # Get dimensions of x and y for emulator
-        params['emulator']['args']['data_n_x'] = data.x_train.shape[1]
-        params['emulator']['args']['data_n_y'] = data.y_train.shape[1]
+        pars_emu['args']['data_n_x'] = data.x_train.shape[1]
+        pars_emu['args']['data_n_y'] = data.y_train.shape[1]
         # Build architecture
         emu.build(
-            params['emulator']['args'],
+            pars_emu['args'],
             data=data,
             verbose=args.verbose)
 
     # Default output parameters
     try:
-        timeout = params['output']['timeout']
+        timeout = pars_out['timeout']
         if args.verbose:
             io.info('Time limit for execution is {} hours'.format(timeout))
     except KeyError:
@@ -157,13 +161,13 @@ def train_emu(args):
 
     # Update number of epochs to run
     if args.resume and args.additional_epochs < 0:
-        epochs = max(params['emulator']['args']['epochs'] - emu.epochs[-1], 0)
+        epochs = max(pars_emu['args']['epochs'] - emu.epochs[-1], 0)
     elif args.resume and args.additional_epochs > 0:
         epochs = args.additional_epochs
     elif args.additional_epochs < 0:
-        epochs = params['emulator']['args']['epochs']
+        epochs = pars_emu['args']['epochs']
     else:
-        epochs = params['emulator']['args']['epochs'] + args.additional_epochs
+        epochs = pars_emu['args']['epochs'] + args.additional_epochs
 
     # Update initial learning rate
     if args.resume and args.learning_rate < 0:
@@ -171,17 +175,17 @@ def train_emu(args):
     elif args.learning_rate > 0:
         learning_rate = args.learning_rate
     else:
-        learning_rate = params['emulator']['args']['learning_rate']
+        learning_rate = pars_emu['args']['learning_rate']
 
     # Train the emulator
     emu.train(
         data,
         epochs,
         learning_rate,
-        patience=params['emulator']['args']['patience'],
-        path=params['output']['path'],
+        patience=pars_emu['args']['patience'],
+        path=pars_out['path'],
         timeout=timeout,
-        reduce_learning_rate=params['emulator']['args']['reduce_learning_rate'],
+        reduce_learning_rate=pars_emu['args']['reduce_learning_rate'],
         get_plots=True,
         verbose=args.verbose)
 
