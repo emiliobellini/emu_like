a2dedf789c9c3a0cf80790875b4544ecee528f58
Emilio Bellini <emilio.bellini@protonmail.com>
2025-11-22 08:07:16 +0100

    .

--- scripts/check_sampling_status.py
+++ scripts/check_sampling_status.py
@@ -1,7 +1,7 @@
 import argparse
-import os
 import re
 import emu_like.io as io
+
 
 def get_head(fname, lines=1):
     """
@@ -21,6 +21,7 @@ def get_head(fname, lines=1):
         line_list = list(f.readlines(total_bytes_scanned))
         line_list = [x.rstrip() for x in line_list[:lines]]
     return line_list
+
 
 def get_tail(fname, lines=1):
     """
@@ -52,7 +53,8 @@ if __name__ == '__main__':
     parser.add_argument('log_files', nargs='+')
     args = parser.parse_args()
 
-    str_out = '\x1b[1;32m[info]\x1b[00m Resuming from '
+    str_out_1 = '\x1b[1;32m[info]\x1b[00m Resuming from '
+    str_out_2 = '\x1b[1;32m----> \x1b[00mWriting output in '
     str_save = '\x1b[1;32m[info]\x1b[00m Saving every '
 
     for log_file in args.log_files:
@@ -64,8 +66,10 @@ if __name__ == '__main__':
         # Locate output file
         head = get_head(log_file, lines=40)
         for line in head:
-            if line.startswith(str_out):
-                output_file = line.replace(str_out, '')[:-1]
+            if line.startswith(str_out_1):
+                output_file = line.replace(str_out_1, '')[:-1]
+            elif line.startswith(str_out_2):
+                output_file = line.replace(str_out_2, '')
             elif line.startswith(str_save):
                 save_interval = line.replace(str_save, '')
                 save_interval = int(save_interval.replace(' steps', ''))
@@ -73,13 +77,17 @@ if __name__ == '__main__':
         # Get samples this run
         tail = get_tail(err_file, lines=1)[0]
         tail = re.search(r'[0-9]+/[0-9]+', tail).group()
-        samples_this_run, remaining_this_run = [int(x) for x in tail.split('/')]
+        samples_this_run, remaining_this_run = [
+            int(x) for x in tail.split('/')]
 
         # Get saved samples
         fits = io.FitsFile(output_file)
         hd = fits.get_header(0)
         sp_name = list(hd['y_model']['outputs'].keys())[0]
-        samples_saved = fits.get_data(sp_name).shape[0]
+        try:
+            samples_saved = fits.get_data(sp_name).shape[0]
+        except KeyError:
+            samples_saved = 0
         samples_tot = fits.get_data('x_data').shape[0]
 
         # Print stuff
@@ -88,7 +96,10 @@ if __name__ == '__main__':
         # io.print_level(1, 'Total samples: {}'.format(samples_tot))
         io.print_level(1, 'Number of samples run: {}'.format(samples_run))
         io.print_level(1, 'Number of samples saved: {}'.format(samples_saved))
-        io.print_level(1, 'Remaining samples to run: {}'.format(samples_tot-samples_run))
-        io.print_level(1, 'Remaining samples to save: {}'.format(samples_tot-samples_saved))
-        io.print_level(1, 'Remaining samples to next save: {}'.format(save_interval-samples_run+samples_saved))
+        io.print_level(1, 'Remaining samples to run: {}'.format(
+            samples_tot-samples_run))
+        io.print_level(1, 'Remaining samples to save: {}'.format(
+            samples_tot-samples_saved))
+        io.print_level(1, 'Remaining samples to next save: {}'.format(
+            save_interval-samples_run+samples_saved))
         print()
